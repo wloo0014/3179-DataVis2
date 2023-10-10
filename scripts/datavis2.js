3c0b37754bcfbaaed20a66933469897c87337e58
Ben Lo <wloo0014@student.monash.edu>
2023-10-10 22:22:14 +1100

    added basic html structure

--- scripts/datavis2.js
+++ scripts/datavis2.js
@@ -1,6 +1,24 @@
 
 // embedding the map
-var mapVis = "script/map.vg.json";
+var mapVis = "scripts/map.vg.json";
 	vegaEmbed('#mapVis', mapVis).then(function(result) {
+		// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+// embedding gdp per year
+var gdp = "scripts/gdp_per_year.vg.json";
+	vegaEmbed('#gdpPerYear', gdp).then(function(result) {
+		// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+// embedding bmi per year
+var bmi = "scripts/bmi_per_year.vg.json";
+	vegaEmbed('#bmiPerYear', bmi).then(function(result) {
+		// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+// embedding gdp vs life expectancy
+var gdpLE = "scripts/gdp_vs_life_expectancy.vg.json";
+	vegaEmbed('#gdpLifeExpectancy', gdpLE).then(function(result) {
 		// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
